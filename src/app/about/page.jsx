ac5eb870768d4c49e7797043eb7ad88ad695fa2b
NachoCurbelo <nachocurbelo2024@gmail.com>
2024-07-30 12:25:18 -0300

    initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,101 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  XIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata = {
+  title: 'About',
+  description:
+    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+          I'm Ignacio Curbelo. Living in Montevideo, Uruguay, I design what's to come.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+            I'm a 29-year-old self-taught freelance graphic designer with five years of experience as a photography editor for a food delivery app company. Now, I'm diving into UX/UI design, combining my previous knowledge with a passion for creating seamless user experiences.
+            </p>
+            <p>
+            Since I was little, I've always been inclined to learn on my own. At the age of six, my curiosity about drummers led me to explore percussion. I built my first drum kit using cans and jars of various sizes, attempting to replicate the rhythms of songs. One day, my father noticed my passion and bought me my first real drum kit.
+            </p>
+            <p>
+            I embraced that proactive learning approach as a lifestyle, enhancing it with university studies in communication and design. Now, I'm an e-learner focused on UX/UI, and day to day boosting my expertise through self-teaching.
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink href="#" icon={XIcon}>
+              Follow on X
+            </SocialLink>
+            <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+              Follow on Instagram
+            </SocialLink>
+            <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="mailto:spencer@planetaria.tech"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              spencer@planetaria.tech
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
---
+++ src/app/uses/page.jsx
@@ -0,0 +1,73 @@
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Uses',
+  description: 'Software I use, gadgets I love, and other things I recommend.',
+}
+
+export default function Uses() {
+  return (
+    <SimpleLayout
+      title="Software I use, gadgets I love, and other things I recommend."
+      intro="I get asked a lot about the things I use to build UI designs, stay productive and get results in time. Here’s a big list of all of my favorite stuff."
+    >
+      <div className="space-y-20">
+        <ToolsSection title="Workstation">
+          <Tool title="Powerfull desktop PC">
+          </Tool>
+          <Tool title="32-inch LG 2K (1440p) monitor">
+          The only display on the market if you want something HiDPI and bigger than 27”. When you’re working at planetary scale, every pixel you can get counts.
+          </Tool>
+          <Tool title="Logitech TKL SE mechanical keyboard">
+          We know that in today's universe speed is worth gold. This keyboard helps me multitask and type at incredible speeds.
+          </Tool>
+          <Tool title="Sony Alpha A7iii">
+          Currently my greatest and latest investment, this camera took my photography and editing skills beyond limits thanks to his amazing fidelity and image quality.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Design">
+          <Tool title="Figma">
+          I started using Figma as just a design tool but now it’s become my
+            virtual whiteboard for plenty of my jobs. Never would have expected
+            the collaboration features to be the real hook
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Productivity">
+          <Tool title="SavvyCal">
+          Great tool for scheduling meetings while protecting my calendar and
+            making sure I still have lots of time for deep work during the week.
+          </Tool>
+          <Tool title="Miro">
+          Miro its like a supercharger to my productivity, It always simplifyed collaboration, planning, and communication with my team members, really leading to better project outcomes.
+          </Tool>
+          <Tool title="Agile">
+          I've been implementing the agile methodology for several years. My previous job taught it to me. I still remembering the day when the company's scrum master came to our team and told us that we could get better and bigger productivity and objetctives results by using that method. Turns out that she was absolutly right. With our photography team we were able to positive scale the company's kpi's and also make restaurants and company's revenue go to the moon.
+          </Tool>
+        </ToolsSection>
+      </div>
+    </SimpleLayout>
+  )
+}
