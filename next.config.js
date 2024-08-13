0818bf0dd08b234a700ee13ed1e3af820ccd143b
NachoCurbelo <nachocurbelo2024@gmail.com>
2024-08-13 17:02:07 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  output: 'export',
+  exportTrailingSlash: true, // Optional, to add trailing slashes to URLs
+  images: {
+    unoptimized: true, // Optional, depends on your usage of images
+  },
+};
