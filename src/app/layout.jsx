41d1f1ead6c592f42c38553da639a7134ce27022
Mateo Nogueira <mateo@havenservicing.com>
2024-08-15 15:32:01 -0300

    fix favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,6 @@ export const metadata = {
   },
   description:
     'I’m Ignacio, graphic and uxui freelance designer, arpiring product designing. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
-  icons: { icon: '/favicon2.ico' },
 }
 
 export default function RootLayout({ children }) {
